0b39273c26ce1d2530bed5bd71404c4e687ad33f
record001 <umarkhidirboev@gmail.com>
2023-07-26 19:14:02 +0500

    fixed gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 import "../css/fonts.css"
 import "../css/globals.css"
 import "../css/address.css"
@@ -21,5 +23,25 @@ import "../css/washProcess.css"
 import "../css/washFold.css"
 import "../css/pickupDeliveryScreen.css"
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <main>
+       <Script src="https://www.googletagmanager.com/gtag/js?id=G-FHQQS49S2W"  strategy="afterInteractive" />
+
+      
+        
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W9RD3PP');`}
+        </Script>
+        
+         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9RD3PP"
+        height={0} width={0} style={{display:"none", visibility:"hidden"}}></iframe></noscript>
+
+      <Component {...pageProps} />
+    </main>
+     
+     )
 }
