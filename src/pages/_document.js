1d3507afa2b17d4a0a89eeddf85ccb5437b8b3ba
record001 <umarkhidirboev@gmail.com>
2023-07-26 17:40:07 +0500

    google tag manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+      
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9RD3PP"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
         <Main />
         <NextScript />
       </body>
